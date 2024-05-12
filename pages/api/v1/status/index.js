665769ea762ad014f85c218b4205c608b6005e55
Lucas Souza <99350331+lucassouzasilvadev@users.noreply.github.com>
2024-05-12 03:08:46 +0000

    feat: add info database api status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as soma;");
-  console.log(result.rows);
-  response.status(200).json({ ludmilo: "deu bom ludmilo" });
+  const updatedAt = new Date().toISOString();
+  const dataBaseVersionResult = await database.query("SHOW server_version");
+  const dataBaseVersion = dataBaseVersionResult.rows[0].server_version;
+  const dataBaseMaxConnectionResult = await database.query(
+    "SHOW max_connections",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionResult.rows[0].max_connections;
+
+  const dataBaseOpennedConnectionResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersion,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: dataBaseOpennedConnectionResult.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
